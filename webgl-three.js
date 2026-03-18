7017c1a316549b994a0f9ef1cd08670532bc0a1e
Johanna <164856597+b23johgu@users.noreply.github.com>
2026-03-18 15:21:45 +0100

    Made floor longer and changed sphere + camera position.. Made lightning a little softer

--- webgl-three.js
+++ webgl-three.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene();
 
 /* Create camera */
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.set(0, 3, 5); // camera position x,y,z coordinates
+camera.position.set(0, 5, 25); // camera position x,y,z coordinates
 camera.lookAt(0, 1, 0); // camera looks at coordinates 0,1,0 (the sphere)
 
 /* WebGL renderer */
@@ -16,7 +16,7 @@ renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement ); // creates a canvas
 
 /* Create light */
-const light = new THREE.HemisphereLight( 0xffffff, 0x080820, 1 ); // overall soft white light shining from top
+const light = new THREE.HemisphereLight( 0xffffff, 0x080820, 0.7 ); // overall soft white light shining from top
 scene.add( light );
 
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 ); // dir light to give more effects to objects, like shadows/shine
@@ -24,7 +24,7 @@ directionalLight.castShadow = true;
 scene.add( directionalLight );
 
 /* Create floor */
-const floorGeometry = new THREE.PlaneGeometry( 5, 5 ); // width , length
+const floorGeometry = new THREE.PlaneGeometry( 10, 40 ); // width , length
 const floorMaterial = new THREE.MeshStandardMaterial( { color: 0xcccccc, side: THREE.DoubleSide } );
 const floor = new THREE.Mesh( floorGeometry, floorMaterial );
 floor.rotation.x = -Math.PI / 2; // rotates floor (default for planegeometry is acting like a wall)
@@ -36,6 +36,7 @@ const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xff00ff , metalness: 0.5, roughness: 0.0 });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 sphere.position.y = 1; //makes sphere appear to be on top of floor
+sphere.position.z = 18; //sphere begins at the nearest part of floor, floor is 40 (20 on each side of middle)
 sphere.castShadow = true;
 scene.add( sphere );
 
