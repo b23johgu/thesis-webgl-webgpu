01a3ebbfe87cbe2a979495eae6931c850294d560
Johanna <164856597+b23johgu@users.noreply.github.com>
2026-03-18 13:06:10 +0100

    Added floor and changed camera position.

--- webgl-three.js
+++ webgl-three.js
@@ -5,6 +5,8 @@ const scene = new THREE.Scene();
 
 /* Create camera */
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.set(0, 3, 5); // camera position x,y,z coordinates
+camera.lookAt(0, 1, 0); // camera looks at coordinates 0,1,0 (the sphere)
 
 /* WebGL renderer */
 const renderer = new THREE.WebGLRenderer();
@@ -18,13 +20,20 @@ scene.add( light );
 
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 ); // dir light to give more effects to objects, like shadows/shine
 scene.add( directionalLight );
+
+/* Create floor */
+const floorGeometry = new THREE.PlaneGeometry( 5, 5 );
+const floorMaterial = new THREE.MeshStandardMaterial( { color: 0xffff00, side: THREE.DoubleSide } );
+const floor = new THREE.Mesh( floorGeometry, floorMaterial );
+floor.rotation.x = -Math.PI / 2; // rotates floor (default for planegeometry is acting like a wall)
+scene.add( floor );
+
 /* Create a sphere */
 const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xff00ff , metalness: 0.5, roughness: 0.0 });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.y = 1; //makes sphere appear to be on top of floor
 scene.add( sphere );
-
-camera.position.z = 5;
 
 /* Render the scene */
 function animate( time ) {
