21fc55efa15efbc033684c4ebe6443e855b5c524
Johanna <164856597+b23johgu@users.noreply.github.com>
2026-03-18 12:15:03 +0100

    Imported three.js and set up a scene containing a magenta sphere
    https://threejs.org/manual/#en/installation & https://threejs.org/manual/#en/creating-a-scene

--- webgl-three.js
+++ webgl-three.js
@@ -1,1 +1,28 @@
 console.log("It's working!");
+
+import * as THREE from 'three';
+
+/* Create scene */
+const scene = new THREE.Scene();
+
+/* Create camera */
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+/* WebGL renderer */
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement ); // creates a canvas
+
+/* Create a sphere */
+const sphereGeometry = new THREE.SphereGeometry(2, 32, 32);
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+scene.add( sphere );
+
+camera.position.z = 5;
+
+/* Render the scene */
+function animate( time ) {
+    renderer.render( scene, camera );
+}
