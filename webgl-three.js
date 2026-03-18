93e0dcc3a6193e0422a8ac1c18cdd26d8584d446
Johanna <164856597+b23johgu@users.noreply.github.com>
2026-03-18 13:19:19 +0100

    Added shadows and changed color of floor

--- webgl-three.js
+++ webgl-three.js
@@ -11,21 +11,24 @@ camera.lookAt(0, 1, 0); // camera looks at coordinates 0,1,0 (the sphere)
 /* WebGL renderer */
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.shadowMap.enabled = true; // Enable shadows
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement ); // creates a canvas
 
 /* Create light */
-const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 ); // overall soft warm light
+const light = new THREE.HemisphereLight( 0xffffff, 0x080820, 1 ); // overall soft white light shining from top
 scene.add( light );
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 ); // dir light to give more effects to objects, like shadows/shine
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 ); // dir light to give more effects to objects, like shadows/shine
+directionalLight.castShadow = true;
 scene.add( directionalLight );
 
 /* Create floor */
-const floorGeometry = new THREE.PlaneGeometry( 5, 5 );
-const floorMaterial = new THREE.MeshStandardMaterial( { color: 0xffff00, side: THREE.DoubleSide } );
+const floorGeometry = new THREE.PlaneGeometry( 5, 5 ); // width , length
+const floorMaterial = new THREE.MeshStandardMaterial( { color: 0xcccccc, side: THREE.DoubleSide } );
 const floor = new THREE.Mesh( floorGeometry, floorMaterial );
 floor.rotation.x = -Math.PI / 2; // rotates floor (default for planegeometry is acting like a wall)
+floor.receiveShadow = true;
 scene.add( floor );
 
 /* Create a sphere */
@@ -33,6 +36,7 @@ const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xff00ff , metalness: 0.5, roughness: 0.0 });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 sphere.position.y = 1; //makes sphere appear to be on top of floor
+sphere.castShadow = true;
 scene.add( sphere );
 
 /* Render the scene */
