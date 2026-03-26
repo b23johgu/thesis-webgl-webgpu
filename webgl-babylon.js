9ba7e7d47e5a9948444cf1d4375d260671c8f4e6
Johanna <164856597+b23johgu@users.noreply.github.com>
2026-03-26 11:34:39 +0100

    Directional light and shadows added. Floor and sphere got colors

--- webgl-babylon.js
+++ webgl-babylon.js
@@ -12,15 +12,33 @@ var createScene = function(){
     camera.setTarget(BABYLON.Vector3.Zero());
 
     /* Create light */
-    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+	var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    light.diffuse = new BABYLON.Color3.FromHexString("#ffffff");
+    light.groundColor = new BABYLON.Color3.FromHexString("#080820");
+    light.intensity = 0.2;
+
+    var dirlight = new BABYLON.DirectionalLight('dirLight', new BABYLON.Vector3(0, -10, 5), scene);
+    light.position = new BABYLON.Vector3(20, 40, 20);
 
     /* Create floor */
-    var ground = BABYLON.MeshBuilder.CreateGround("ground1", { width: 10, height: 40, subdivisions: 2, updatable: false }, scene);
+    var floor = BABYLON.MeshBuilder.CreateGround("ground1", { width: 10, height: 40, subdivisions: 2, updatable: false }, scene);
+    const floorMat = new BABYLON.StandardMaterial("floorMat");
+    floorMat.diffuseColor = new BABYLON.Color3.FromHexString("#cccccc");
+    floor.material = floorMat;
 
     /* Create a sphere */
     var sphere = BABYLON.MeshBuilder.CreateSphere('sphere1', {segments: 16, diameter: 2, sideOrientation: BABYLON.Mesh.FRONTSIDE}, scene);
+    const sphereMat = new BABYLON.StandardMaterial("sphereMat");
+    sphereMat.diffuseColor = new BABYLON.Color3.FromHexString("#ff00ff");
+    sphere.material = sphereMat;
+
     sphere.position.y = 1; //makes sphere appear to be on top of floor
-    sphere.position.z = -19;
+    sphere.position.z = -19; // sphere start position
+
+    /* Shadows */
+    var shadowGenerator = new BABYLON.ShadowGenerator(1024, dirlight);
+    floor.receiveShadows = true; // floor recieves shadow
+    shadowGenerator.getShadowMap().renderList.push(sphere); // sphere makes shadow
 
     return scene;
 }
