ca84289e0e9ab35f28ba7c007de6e1929ec8de09
Johanna <164856597+b23johgu@users.noreply.github.com>
2026-03-26 10:39:23 +0100

    Start of Babylon version with floor and sphere. Css to display simulation in whole window of webpage
    Inspired by code of Babylon playground https://playground.babylonjs.com/#

--- webgl-babylon.js
+++ webgl-babylon.js
@@ -1,1 +1,38 @@
-console.log("It's working!");
+import * as BABYLON from 'babylonjs';
+
+var canvas = document.getElementById('renderCanvas');
+var engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
+
+var createScene = function(){
+    /* Create scene */
+    var scene = new BABYLON.Scene(engine);
+
+    /* Create camera */
+    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -30), scene);
+    camera.setTarget(BABYLON.Vector3.Zero());
+
+    /* Create light */
+    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+
+    /* Create floor */
+    var ground = BABYLON.MeshBuilder.CreateGround("ground1", { width: 10, height: 40, subdivisions: 2, updatable: false }, scene);
+
+    /* Create a sphere */
+    var sphere = BABYLON.MeshBuilder.CreateSphere('sphere1', {segments: 16, diameter: 2, sideOrientation: BABYLON.Mesh.FRONTSIDE}, scene);
+    sphere.position.y = 1; //makes sphere appear to be on top of floor
+    sphere.position.z = -19;
+
+    return scene;
+}
+
+/* Render the scene */
+var scene = createScene();
+
+engine.runRenderLoop(function(){
+    scene.render();
+});
+
+// canvas/window resize event handler
+window.addEventListener('resize', function(){
+    engine.resize();
+});
